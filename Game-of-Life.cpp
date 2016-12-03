32109fb5ad992876c3be5aaefea1d8dc74d66a30
Juliette Regimbal <juliette.regimbal@gmail.com>
2016-12-03 00:58:28 -0500

    set x and y size of board

--- Game-of-Life.cpp
+++ Game-of-Life.cpp
@@ -7,12 +7,16 @@ int main(int argc, char **argv)
 {
 	int nflag = 0;
 	int rflag = 0;
+	int xflag = 0;
+	int yflag = 0;
+	char *xvalue = NULL;
+	char *yvalue = NULL;
 	char *nvalue = NULL;
 	char *rvalue = NULL;
 	int c;
 	opterr = 0;
 
-	while((c = getopt(argc, argv, "n:")) != -1)
+	while((c = getopt(argc, argv, "n:r:x:y:")) != -1)
 		switch (c)
 		{
 			case 'n':
@@ -23,6 +27,14 @@ int main(int argc, char **argv)
 				rflag = 1;
 				rvalue = optarg;
 				break;
+			case 'x':
+				xflag = 1;
+				xvalue = optarg;
+				break;
+			case 'y':
+				yflag = 1;
+				yvalue = optarg;
+				break;
 			default:
 				break;
 		}
@@ -32,7 +44,8 @@ int main(int argc, char **argv)
 
 	int x, y;
 	x = y = (nflag == 1) ? atoi(nvalue) : 10;
-
+	x = (xflag == 1) ? atoi(xvalue) : x;
+	y = (yflag ==1) ? atoi(yvalue) : y;
 	int ratio = (rflag == 1) ? atoi(rvalue) : 1;
 
 	Board board(x, y, ratio);
